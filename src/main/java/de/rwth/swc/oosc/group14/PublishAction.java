b716917d02ace9c6a924bf4c61e93547acfcc3c4
Atang <tlc314960@gmail.com>
2022-01-22 11:09:54 +0100

    1.22

--- src/main/java/de/rwth/swc/oosc/group14/PublishAction.java
+++ src/main/java/de/rwth/swc/oosc/group14/PublishAction.java
@@ -1,6 +1,9 @@
 package de.rwth.swc.oosc.group14;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import de.rwth.swc.oosc.group14.AppLabels;
 import org.jhotdraw.annotation.Nullable;
 import org.jhotdraw.app.Application;
@@ -19,6 +22,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 public class PublishAction extends AbstractViewAction {
@@ -67,18 +72,16 @@ public class PublishAction extends AbstractViewAction {
             os = connection.getOutputStream();
 
             ObjectMapper mapper = new ObjectMapper();
+            JavaTimeModule javaTimeModule=new JavaTimeModule();
+            // Hack time module to allow 'Z' at the end of string (i.e. javascript json's)
+            javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ISO_DATE_TIME));
+            mapper.registerModule(javaTimeModule);
+            mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+//            System.out.println(mapper.writeValueAsString(image));
             os.write(mapper.writeValueAsString(image).getBytes());
-//            if (connection.getResponseCode() == 200) {
-//                is = connection.getInputStream();
-//                if (null != is) {
-//                    br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-//                    String temp = null;
-//                    while (null != (temp = br.readLine())) {
-//                        result.append(temp);
-//                        result.append("\r\n");
-//                    }
-//                }
-//            }
+            if (connection.getResponseCode() == 200) {
+                System.out.println("success");
+            }
 
 
         } catch (MalformedURLException e1) {
